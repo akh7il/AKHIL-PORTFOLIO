4101d25a0cbd57aba802f2de53f73cb267206872
akh7il <akhilprsnl01@gmail.com>
2025-12-05 07:54:07 +0530

    solved templates path issue

--- AKHIL_PORTFOLIO/settings.py
+++ AKHIL_PORTFOLIO/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'AKHIL_PORTFOLIO.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'AKHIL_PORTFOLIO_APP', ' TEMPLATES ')],
+        'DIRS': [os.path.join(BASE_DIR, 'AKHIL_PORTFOLIO_APP', ' TEMPLATES')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
